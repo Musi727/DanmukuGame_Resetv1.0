cf6df7f973c0eb664e00a98ec880a26ab87fd5d7
Musi727 <f475961029@gmail.com>
2024-01-15 08:46:09 +0800

    初始版本提交

---
+++ DanmukuGame_Resetv1.0/Assets/Scripts/Game.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //调用UIMgr生成开始界面
+        // UIMgr.Instance.ShowPanel<BeginPanel>("BeginPanel",E_UI_Layer.mid,(panel)=>{});
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
